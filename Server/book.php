61ee306ff270640d5b48be57163aac6ce92f1e84
Nabin Paudel <paudelnabin11@gmail.com>
2023-07-11 14:17:26 +0545

    Demo booking

---
+++ Server/book.php
@@ -0,0 +1,25 @@
+<?php
+include("../include/dbcon.php"); 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $date = $_POST['date'];
+  $time = $_POST['time'];
+
+  // Check if the date and time are already booked
+  $sql = "SELECT * FROM bookings WHERE date = '$date' AND time = '$time'";
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result) > 0) {
+    echo "Unavailable";
+  } else {
+    $sql = "INSERT INTO bookings (date, time) VALUES ('$date', '$time')";
+    if (mysqli_query($conn, $sql)) {
+      echo "Booking successful!";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  }
+
+  mysqli_close($conn);
+}
+?>
---
+++ include/dbcon.php
@@ -0,0 +1,20 @@
+<?php
+
+//connect to database
+$dbuser="root";
+$dbpass="";
+$dbhost="localhost";
+$dbname="futsal";
+
+
+//create a connection
+$conn=new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+
+
+//check connection
+if ($conn->connect_error){
+    echo("Connection error!!");
+}
+
+
+?>
