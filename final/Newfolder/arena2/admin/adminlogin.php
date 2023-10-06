31ad209dd1179cc9cef73bc24edb24f9ee74f2cd
Nabin Paudel <paudelnabin11@gmail.com>
2023-10-06 20:52:10 +0545

    update

--- final/Newfolder/arena2/admin/adminlogin.php
+++ final/Newfolder/arena2/admin/adminlogin.php
@@ -8,17 +8,44 @@ if(isset($_SESSION['username'])){
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <link rel="icon" href="Artboard 2.png">
+    <title>Login</title>
+    <link href="adminlogin.css" rel="stylesheet">
 </head>
 
-<body>
-    <form action="adminloginprocess.php" method="POST">
-        <label>Username : </label><input type="text" name="username" placeholder="username"><br><br>
-        <label>Password : </label><input type="password" name="password" placeholder="passowrd"><br><br>
-        <button type="submit" name="login">Log in</button> 
-    </form>
+
+<body>    
+    <div class="wrapper">
+        <div class="form-box login">
+            <h2>Admin Login</h2>
+            <form action="adminloginprocess.php" method="post">
+                <?php
+                // Display login error message if there is one
+                if (!empty($error_message)) {
+                    echo '<div style="color: red;">' . $error_message . '</div>';
+                }
+                ?>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                    <input type="text" name="username" required>
+                    <label>Username</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                    <input type="password" name="password" required>
+                    <label>Password</label>
+                </div>
+                <div>
+                    <!-- <a class="forgot" href="forgotpassword.php">forgot password?</a> -->
+                </div>
+                <button type="submit" class="btn">Login</button>
+                
+            </form>
+        </div>
+    </div>
+    <script src="login.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
