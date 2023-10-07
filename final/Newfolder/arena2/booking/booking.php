56ee54ae7c72d51f602788eba5c5744d6c2bc0f9
Nabin Paudel <paudelnabin11@gmail.com>
2023-10-07 18:54:40 +0545

    Database

--- final/Newfolder/arena2/booking/booking.php
+++ final/Newfolder/arena2/booking/booking.php
@@ -42,79 +42,89 @@ if (!isset($_SESSION['SN'])) {
       <h1>The bookings are currently disabled by the admin.</h1>
       "
   ?>
-    <h2>Message from Admin :  <?php
-                            echo $row['message'];
-                            ?></h2>
+    <h2>Message from Admin : <?php
+                              echo $row['message'];
+                              ?></h2>
   <?php
     echo "<h3>For more information, <a href='../index.php#contact'>contact</a> the admin.</h3>
     </div>
   </div>";
-  }else{
+  } else {
 
   ?>
-  <div class="wrapper">
-    <div class="form-box login">
-      <h1>Futsal Booking</h1>
-      <form action="book.php" method="POST">
-        <div class="calender">
-          <div>
-            <!-- <label for="time">Date:</label> -->
-            <input class="date" name="date" type="date" required>
-          </div>
-          <div>
-            <!-- <input class="time" name="time" type="time" required> -->
-            <!-- <label for="time">Time:</label> -->
-            <select name="time" class="time" id="time" required>
-              <option value="9:00">9:00-10:00</option>
-              <option value="10:00">10:00-11:00</option>
-              <option value="11:00">11:00-12:00</option>
-              <option value="12:00">12:00-13:00</option>
-              <option value="13:00">13:00-14:00</option>
-              <option value="14:00">14:00-15:00</option>
-              <option value="15:00">15:00-16:00</option>
-              <option value="16:00">16:00-17:00</option>
-              <option value="17:00">17:00-18:00</option>
-              <option value="18:00">18:00-19:00</option>
-            </select>
-          </div>
-          <div><button class="btn submit">Submit</button></div>
-        </div>
-        <div class="reg">
-          <a class="register-link" href="#">My bookings</a>
-        </div>
-      </form>
-    </div>
-    <div class="form-box register">
-      <h1>My Bookings</h1>
-      <form action="sucess/registersucess.php" method="POST">
-        <div class="log">
+    <div class="wrapper">
+      <div class="form-box login">
+        <h1>Book here</h1>
+        <form action="book.php" method="POST">
+          <div class="calender">
+            <div>
+              <!-- <label>Date:</label> -->
+              <input class="date" id="eventDate" name="date" type="date" required>
+              <script>
+                // Get the input element
+                const eventDateInput = document.getElementById("eventDate");
 
-          <!-- display bookings -->
-          <?php
-          include('../include/dbcon.php');
-          if (!isset($_SESSION['SN'])) {
-            header('Location: login.php');
-            exit();
-          }
-          $SN = $_SESSION['SN'];
-          $sql = "SELECT * FROM bookings WHERE SN = '$SN' AND date >= CURDATE() order by date asc ";
-          $result = $conn->query($sql);
-          if ($result->num_rows > 0) {
-            echo "<table><tr><th>Date</th><th>Time</th><th></th></tr>";
-            while ($row = $result->fetch_assoc()) {
-              echo "<tr><td>" . $row["date"] . "</td><td>" . $row["time"] . "</td><td><a href='cancelbookings.php?SN=" . $row["SN"] . "&date=" . $row["date"] . "&time=" . $row["time"] . "'>Cancel</a></td></tr>";
+                // Get today's date in YYYY-MM-DD format
+                const today = new Date().toISOString().split('T')[0];
+
+                // Set the minimum date
+                eventDateInput.min = today;
+              </script>
+            </div>
+            <div>
+              <!-- <input class="time" name="time" type="time" required> -->
+              <!-- <label>Time:</label> -->
+              <select name="time" class="time" id="time" required>
+                <option value="9:00">9:00-10:00</option>
+                <option value="10:00">10:00-11:00</option>
+                <option value="11:00">11:00-12:00</option>
+                <option value="12:00">12:00-13:00</option>
+                <option value="13:00">13:00-14:00</option>
+                <option value="14:00">14:00-15:00</option>
+                <option value="15:00">15:00-16:00</option>
+                <option value="16:00">16:00-17:00</option>
+                <option value="17:00">17:00-18:00</option>
+                <option value="18:00">18:00-19:00</option>
+              </select>
+            </div>
+            <div><button class="btn submit">Submit</button></div>
+          </div>
+          <div class="reg">
+            <a class="register-link" href="#">My bookings</a>
+          </div>
+        </form>
+      </div>
+      <div class="form-box register">
+        <h1>My Bookings</h1>
+        <form action="sucess/registersucess.php" method="POST">
+          <div class="log">
+
+            <!-- display bookings -->
+            <?php
+            include('../include/dbcon.php');
+            if (!isset($_SESSION['SN'])) {
+              header('Location: login.php');
+              exit();
             }
-            echo "</table>";
-          } else {
-            echo "No bookings found.";
-          }
-          $conn->close();
-          ?>
-          <a class="login-link" href="#">Go Back</a>
-        </div>
-      </form>
+            $SN = $_SESSION['SN'];
+            $sql = "SELECT * FROM bookings WHERE SN = '$SN' AND date >= CURDATE() order by date asc ";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+              echo "<table><tr><th>Date</th><th>Time</th><th></th></tr>";
+              while ($row = $result->fetch_assoc()) {
+                echo "<tr><td>" . $row["date"] . "</td><td>" . $row["time"] . "</td><td><a href='cancelbookings.php?SN=" . $row["SN"] . "&date=" . $row["date"] . "&time=" . $row["time"] . "'>Cancel</a></td></tr>";
+              }
+              echo "</table>";
+            } else {
+              echo "No bookings found.";
+            }
+            $conn->close();
+            ?>
+            <a class="login-link" href="#">Go Back</a>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
   <?php
   }
   ?>
---
+++ final/Newfolder/arena2/include/futsal.sql
@@ -0,0 +1,132 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 07, 2023 at 03:03 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `futsal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `State` varchar(11) NOT NULL,
+  `message` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `State`, `message`) VALUES
+(1, 'admin', 'admin', 'enabled', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `id` int(11) NOT NULL,
+  `date` varchar(20) NOT NULL,
+  `time` varchar(20) NOT NULL,
+  `SN` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE `register` (
+  `SN` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` bigint(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_register` (`SN`);
+
+--
+-- Indexes for table `register`
+--
+ALTER TABLE `register`
+  ADD PRIMARY KEY (`SN`),
+  ADD UNIQUE KEY `phone` (`phone`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
+--
+-- AUTO_INCREMENT for table `register`
+--
+ALTER TABLE `register`
+  MODIFY `SN` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `fk_register` FOREIGN KEY (`SN`) REFERENCES `register` (`SN`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
